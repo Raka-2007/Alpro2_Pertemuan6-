fc31cc1e275d3a477560a3de4cd13364da42a1c0
Raka <remillulfata@gmail.com>
2026-05-02 11:31:17 +0700

    Add files via upload

---
+++ latihan1.py
@@ -0,0 +1,20 @@
+#Raka Emillul Fata
+#Latihan 1 : Implementasi Bubble Sort
+
+def bubble_sort(data):
+    n = len(data)
+    count = 0
+
+    for i in range(n):
+        for j in range(0, n-i-1):
+            count += 1
+            if data[j]>data[j+1]:
+                data[j],data[j+1]=data[j+1],data[j]
+
+    return data, count
+data = [5,2,9,1,5,6]
+hasil,langkah = bubble_sort(data.copy())
+
+print("Data: ", data)
+print("Hasil: ", hasil)
+print("Jumlah perbandingan: ", langkah)
